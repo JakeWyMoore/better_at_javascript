da1010452495595e2d8b2db84e8242d301f30a4c
Jacob Moore <jakewymoore@gmail.com>
2021-11-19 13:59:36 -0700

    first commit

---
+++ basics.js
@@ -0,0 +1,23 @@
+// VARIABLES
+
+// Create a Variable: var
+var favoriteFood = 'pizza'
+var numOfSlices = 8
+
+console.log(favoriteFood)
+console.log(numOfSlices)
+
+// Create a Variable: let
+let changeMe = 'Change from this'
+console.log(changeMe)
+
+changeMe = 'To this!!!'
+console.log(changeMe)
+
+// Create a Variable: const
+const entree = 'tacos'
+console.log(entree)
+
+// THIS WILL GIVE A TypeError: Assignment to constant variable.
+// entree = 'chicken'
+// console.log(entree)
